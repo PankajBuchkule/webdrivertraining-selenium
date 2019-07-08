a101e34c7dddbbb79747e7fd0a1499daebb0ffdf
Pankaj Buchkule <pankajbuchkule@gmail.com>
2019-07-08 12:36:11 +0530

    Changes - v0.01

--- test/scripts/MyFirstTestNGTest.java
+++ test/scripts/MyFirstTestNGTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 
-public class MyFirstTestNGTest {
+public class MyFirstTestNGTest {                           //Run Program Using TestNG framwork
 	WebDriver driver;
 	@Test
   public void loginTest() {
